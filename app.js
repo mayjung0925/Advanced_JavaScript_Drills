ae9267833cb6fdcb57b0b41f5f1c93e14a2e63fb
Eunjin Jeoung <mayjung0925@gmail.com>
2022-02-28 11:18:32 -0600

    Scoping part No.14-23

--- app.js
+++ app.js
@@ -43,24 +43,50 @@ function printFruits(){
     let favFruit = fruits[2];
     console.log(fruits[0]);
     
+    return function printFavFruit(){
+        let leastFav = fruits[1] ;
+        console.log(favFruit)
+    }
+
+    console.log(leastFav);
+    
+    
 }
 
 //7. I see the first fruit printed in the console
-printFruits()
+printFruits()()
 
 //8. Declare another function that will print your favorite fruit
 
-function printFavFruit(){
-    console.log(favFruit)
-}
 
 //10 why was the printFavFruit function able to log favFruit? 
 //I'm not sure but when the printFruits was called earlier , it might assign the value of the variable favFruit inside the function to the variable faFruit in the global scope 
-printFavFruit();
 
 //11. Remove the declaration of favFruit in the global scope
 //12. Declare favFruit using the let keyword in the printFruits function.
 // ---> printFavFruit() does not have access to the favFruit variable and is now undefined.
 
+
+//14.Nest the printFavFruit function inside of the printFruits function.
+//15.Call the printFavFruit function inside printFruits function
+//16.Save and refresh the browser. favFruit is now logged because the printFavFruit has access to variables in its parent function.
+//17.Create a new variable named leastFav using the let keyword, declare it in the scope of the printFavFruit function.
+//19.Log leastFav to the console after the printFavFruit function is declared-->leastFav log should throw an error because the parent function does not have access to variables declared within the nested function.
+
+//21.Create a new function 
+
+greeting()
+function greeting(){
+    console.log('Hello'+name);
+}
+
+//22.Create a new function
+//23.Call the function before it is declared as an expression, what happens? Because of hoisting and the use the ES6 an error may occur. Adjust the code to allow the function to run.
+
+
+let showText = function (){
+    alert("It's sunny again!")
+}
+showText()
 
 
