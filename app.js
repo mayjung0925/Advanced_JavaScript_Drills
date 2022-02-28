e77ba7e964247797658f57216d8fbead5043aee5
Eunjin Jeoung <mayjung0925@gmail.com>
2022-02-28 10:18:38 -0600

    first try

---
+++ app.js
@@ -0,0 +1,8 @@
+//Hoisting and Functions
+//1. set the name
+name = 'Eunjin';
+//2.Use var to define a variable
+var name;
+//3. Log the value of name --> I expected 'Eunnin ' to be logged to the console
+console.log(name);
+//4. I saw currnet name printed in the console
