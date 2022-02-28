bfe423a1902626f91205883a04eebcb0971bfac3
Eunjin Jeoung <mayjung0925@gmail.com>
2022-02-28 10:34:45 -0600

    finished Hoisting and Functions part

--- app.js
+++ app.js
@@ -6,3 +6,27 @@ var name;
 //3. Log the value of name --> I expected 'Eunnin ' to be logged to the console
 console.log(name);
 //4. I saw currnet name printed in the console
+//5. var - >let
+//7. let->var ,  Create a function, call the function
+//8. I expected the value of name variabele,Covalence, to be logged to the console
+setName()
+ function setName(){
+     var name = 'Covalence';
+     console.log(name);
+ }
+
+ console.log('some text')
+
+ let avg = findAvg(2,2);
+
+ console.log('some text', avg);
+
+ function findAvg(a, b){
+     console.log('some text');
+     var answer = ( a + b )/2;
+     return answer;
+
+ }
+ //11. In what order will the logs be printed?
+ //Firt Line 18 ->Second Line 25  when findAvg is called at line 20 ->Third Line 22
+
